2c4e5975285e6ee2b464afc50b7959306d3741a7
luocaca <471474615@qq.com>
2019-09-09 15:28:36 +0800

    1.    增加自定义view lib

---
+++ wedget/src/main/java/com/just/rebate/wedget/Heel.java
@@ -0,0 +1,26 @@
+package com.just.rebate.wedget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * 自定义view
+ */
+public class Heel extends TextView {
+    public Heel(Context context) {
+        super(context);
+    }
+
+    public Heel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Heel(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public Heel(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
