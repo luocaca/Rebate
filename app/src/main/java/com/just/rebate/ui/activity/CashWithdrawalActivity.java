6c2f2459089e9edc6b8112140991c685dbff0b56
chenmingzhi <539940199@qq.com>
2019-09-16 13:39:20 +0800

    1.UI修改         2.UI细节修改         3.制作个人页面         3.制作充值界面         3.制作账单界面添加数据         4.制作账单详情界面解决跳转bug         5.完成各项界面         6.添加下拉刷新         7.添加个人栏数据         8.更改订单布局         9.界面修改         10.界面微调         11.微调         12.制作支付密码界面

--- app/src/main/java/com/just/rebate/ui/activity/CashWithdrawalActivity.java
+++ app/src/main/java/com/just/rebate/ui/activity/CashWithdrawalActivity.java
@@ -1,16 +1,47 @@
 package com.just.rebate.ui.activity;
 
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.just.rebate.R;
+import com.just.rebate.wedget.PwdEditText;
 
 public class CashWithdrawalActivity extends AppCompatActivity {
+    private Button Btn_Dialog;
+    private PwdEditText pwdEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cash_withdrawal);
+        Btn_Dialog=findViewById(R.id.dialog_cashwwithdrawal);
+        Btn_Dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog();
+            }
+        });
+
+
+    }
+    private void AlertDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(CashWithdrawalActivity.this);
+        LayoutInflater inflater=LayoutInflater.from(CashWithdrawalActivity.this);
+        View view =inflater.inflate(R.layout.dialog_cash_with_drawal,null);
+        final AlertDialog alertDialog=builder.create();
+        alertDialog.show();
+        alertDialog.getWindow().setContentView(view);
+        pwdEditText=view.findViewById(R.id.pwdEditText);
+
+
+
+
+
+
     }
 }
