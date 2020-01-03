936a6d06f9ebbf44193d4b7fd34af1a5617961f8
luocaca <471474615@qq.com>
2020-01-03 16:04:44 +0800

    1.  首页完成修复bug。垃圾离任的bug

--- app/src/main/java/com/just/rebate/adapter/recycle/HomeExpandableItemAdapter.java
+++ app/src/main/java/com/just/rebate/adapter/recycle/HomeExpandableItemAdapter.java
@@ -3,15 +3,18 @@ package com.just.rebate.adapter.recycle;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 
+import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.chad.library.adapter.base.entity.IExpandable;
 import com.chad.library.adapter.base.entity.MultiItemEntity;
 import com.just.rebate.R;
 import com.just.rebate.entity.HomeItem;
+import com.just.rebate.entity.PlatformsBean;
 
 import java.util.List;
 
@@ -43,8 +46,14 @@ public class HomeExpandableItemAdapter extends BaseMultiItemQuickAdapter<MultiIt
     protected void convert(@NonNull BaseViewHolder helper, MultiItemEntity item) {
 
         System.out.println("is head ? " + item.getItemType());
-
-
+        if (item.getItemType() == 0) {
+            HomeItem item1 = (HomeItem) item;
+            helper.setText(R.id.header, item1.Name);
+        } else {
+            PlatformsBean item1 = (PlatformsBean) item;
+            helper.setText(R.id.tv, item1.Name);
+            Glide.with(mContext).load(item1.Logo).into((ImageView) helper.getView(R.id.iv));
+        }
 
 
     }
